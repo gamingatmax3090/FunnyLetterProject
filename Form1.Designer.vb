921dae1b7d273578701f787a9576440d92540677
Admin <Admin@MASON-PC>
2023-01-14 14:06:10 -0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Generate = New System.Windows.Forms.Button()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(146, 150)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PlaceholderText = "Input text here..."
+        Me.TextBox1.Size = New System.Drawing.Size(452, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Generate
+        '
+        Me.Generate.Location = New System.Drawing.Point(240, 200)
+        Me.Generate.Name = "Generate"
+        Me.Generate.Size = New System.Drawing.Size(335, 23)
+        Me.Generate.TabIndex = 1
+        Me.Generate.Text = "Generate text"
+        Me.Generate.UseVisualStyleBackColor = True
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Location = New System.Drawing.Point(604, 148)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(75, 23)
+        Me.btnCopy.TabIndex = 2
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.Generate)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Funny sentance maker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Generate As Button
+    Friend WithEvents btnCopy As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Shared Sub Main()
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1())
+    End Sub
+
+    Public Sub Generate_Click(sender As Object, e As EventArgs) Handles Generate.Click
+        Dim originalText As String = TextBox1.Text
+        Dim modifiedText As String = ""
+        Dim rand As New Random()
+
+        For Each c As Char In originalText
+            If Char.IsLetter(c) Then
+                If rand.Next(10) <= 4 Then
+                    modifiedText += Char.ToUpper(c)
+                Else
+                    modifiedText += Char.ToLower(c)
+                End If
+            Else
+                modifiedText += c
+            End If
+        Next
+        TextBox1.Text = modifiedText
+        btnCopy.Visible = True
+
+    End Sub
+
+    Private Sub btnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
+        Clipboard.SetText(TextBox1.Text)
+    End Sub
+
+End Class
